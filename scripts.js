1270b7d3e4f2832405e954d01d90dc45a96d137a
Oliver Jenkins <cyberforge1@gmail.com>
2024-03-24 10:46:03 +1100

    addition of the dynamic menu extension windows

--- scripts.js
+++ scripts.js
@@ -1,1 +1,30 @@
-//console.log('hello world')
+console.log('hello world')
+
+// JS VARIABLE ASSIGNMENT
+
+let startButton = document.getElementById('start-button')
+let startMenu = document.getElementById('start-menu')
+
+let = document.querySelectorAll('.menu-item')
+
+
+
+// EVENT LISTENERS
+
+startButton.addEventListener('click', toggleStartMenu);
+
+
+
+// FUNCTION DEFINITION
+
+
+// START MENU VISIBILITY TOGGLE
+
+function toggleStartMenu() {
+    // Toggle the display property of the start menu
+    if (startMenu.style.display === 'none') {
+        startMenu.style.display = 'flex';
+    } else {
+        startMenu.style.display = 'none';
+    }
+}
